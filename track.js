5bd25d29dea74cbac1367b09710d9f64afa6c2cf
Chris Whitty <crswty@users.noreply.github.com>
2019-02-06 22:53:55 +0000

    Start up chrome, lauch a user chosen extension and a click tracking extension for user research

---
+++ track.js
@@ -0,0 +1,37 @@
+
+
+document.addEventListener('click', function (event) {
+  if (!localStorage.getItem('clicks')) {
+    localStorage.setItem('clicks', JSON.stringify([]));
+  }
+
+  let currentHistory = JSON.parse(localStorage.getItem('clicks'));
+
+  currentHistory.push(`${event.target.innerText}\t-\t${event.target.className}`);
+  localStorage.setItem('clicks', JSON.stringify(currentHistory));
+});
+
+
+
+function handleKeyup(e) {
+
+  if(e.ctrlKey && e.keyCode === 72) {
+    downloadHistory()
+  }
+}
+
+document.addEventListener('keyup', handleKeyup)
+
+function downloadHistory() {
+
+  let currentHistory = JSON.parse(localStorage.getItem('clicks'));
+  let content = "name\t-\tclassName" + currentHistory.join("\n")
+
+  var atag = window.document.createElement('a');
+  atag.href = window.URL.createObjectURL(new Blob([content], {type: 'text'}))
+  atag.download = 'research-steps.txt'
+
+  document.body.appendChild(atag);
+  atag.click();
+  document.body.removeChild(atag);
+};
